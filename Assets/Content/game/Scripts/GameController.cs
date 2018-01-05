cfb7d173e4aba273cff10188f7dff1a1e3d9ac8d
MGRich <nothing>
2018-01-05 17:29:26 -0600

    pushing everything
    nothing works i have to do this

---
+++ Assets/Content/game/Scripts/GameController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject pauseIndicator;
+
+    void Awake()
+    {
+		bool paused = Input.GetKey(KeyCode.P);
+		if (paused)
+		{
+			Time.timeScale = 0f;
+			AudioListener.pause = true;
+			pauseIndicator.SetActive(true);
+		}
+		else
+		{
+			pauseIndicator.SetActive(false);
+			Time.timeScale = 1f;
+			AudioListener.pause = false;
+		}
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            RestartLevel();
+        }
+    }
+
+    void OnApplicationFocus(bool hasFocus)
+    {
+        if (!hasFocus)
+        {
+            Time.timeScale = 0f;
+            AudioListener.pause = true;
+            pauseIndicator.SetActive(true);
+        }
+        else
+        {
+            pauseIndicator.SetActive(false);
+            Time.timeScale = 1f;
+            AudioListener.pause = false;
+        }
+    }
+
+    public void RestartLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+}
